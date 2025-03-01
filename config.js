90c6955f2911bd66dd662e8476c86cb7cbf3c6da
induwra <induwaraisira793@gmail.com>
2025-03-01 10:20:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Y28j0awL#NAWK6UT9DHcyCPXJqmHScJ-GblA1tbaR0RcNQl7l2qE",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2vR3Babb#YrLtZMfQ1JHYyfck1kwOSXWHRaUcKX4zB5Thfzi9yP4",
 MONGODB: process.env.MONGODB || "mongodb+srv://Maher:Zubair@sigma-male.ggwx4gc.mongodb.net/?retryWrites=true&w=majority",
   MODE :  process.env.MODE || "", // PUT PUBLIC OR PRIVATE
 };
